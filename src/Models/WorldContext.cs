2678d7791e93bc43c9a93b40ef94ba7af79912fb
Mubarak Imam <mubarakadeimam@gmail.com>
2017-07-09 17:53:25 +0100

    preparing for migrations

---
+++ src/Models/WorldContext.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TheWorld.Models
+{
+    public class WorldContext : DbContext
+    {
+        private IConfiguration _config;
+
+        public WorldContext(DbContextOptions options, IConfiguration config) : base(options)
+        {
+            _config = config;
+        }
+
+        public WorldContext()
+        {
+
+        }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var conn = _config.GetConnectionString("DefaultConnection");
+            //optionsBuilder.UseSqlite(_config["ConnectionStrings__DefaultConnection"]);
+            //optionsBuilder.UseSqlite("Data Source=WorldData.sqlite");
+            optionsBuilder.UseSqlite(conn);
+            Console.WriteLine(conn);
+        }
+    }
+}
