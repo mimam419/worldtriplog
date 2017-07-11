5939a61d0fb4337a91d6bf14ba73d665f1f38285
Mubarak Imam <mubarakadeimam@gmail.com>
2017-07-11 13:32:29 +0100

    tested status code automated responses

--- src/Controllers/Api/TripsController.cs
+++ src/Controllers/Api/TripsController.cs
@@ -7,9 +7,9 @@ namespace TheWorld.Controllers.Api
     public class TripsController : Controller
     {
         [HttpGet("api/trips")]
-        public JsonResult Get()
+        public IActionResult Get()
         {
-            return Json(new Trip() { Name = "My Trip" });
+            return Ok(new Trip() { Name = "My Trip" });
         }
     }
 }
